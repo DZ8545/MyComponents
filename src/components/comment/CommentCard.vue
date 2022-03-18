c462f3dcf3d118b205a24a048584e18c18bc6135
DZ <2119921883@qq.com>
2022-03-18 23:40:51 +0800

    first commit

---
+++ src/components/comment/CommentCard.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "CommentCard"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/comment/Release.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Release"
+}
+</script>
+
+<style scoped>
+
+</style>
